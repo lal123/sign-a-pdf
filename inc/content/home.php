bd8015526278e863e704b37ea2628b7cc38dd2d5
lal123 <alain23@gmail.com>
2026-03-31 13:23:45 +0200

    30 march version

--- inc/content/home.php
+++ inc/content/home.php
@@ -6,15 +6,9 @@ switch($action) {
         $img_dir = getcwd() . '/' . UPLOAD_DIR . '/img';
         $img_numb = 0;
         while(file_exists($img_dir . '/' . $pdf_id . '-' . ($img_numb + 1) . '.png')) {
-            echo '<img src="/' . UPLOAD_DIR . '/img/' . $pdf_id . '-' . ($img_numb + 1) . '.png' . '" alt="" border= "0" />';
-
+            echo '<img style="max-width: 500px;" src="/' . UPLOAD_DIR . '/img/' . $pdf_id . '-' . ($img_numb + 1) . '.png' . '" alt="" border= "0" />';
             $img_numb++;
         }
-        echo "[pdf_id][{$pdf_id}][img_seq][{$img_numb}]";
-
-
-
-
         break;
 
     default:
