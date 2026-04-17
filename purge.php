5cc6863f8da5cc33f732bd1569a843dc98ec6f33
lal123 <alain23@gmail.com>
2026-04-17 17:55:47 +0200

    new purge script with sign files

--- purge.php
+++ purge.php
@@ -3,7 +3,11 @@
 chdir('/var/www/sign-a-pdf');
 
 #liste des dossiers à purger
-$dir_list=array('uploads/pdf','uploads/img', 'uploads/sign');
+$dir_list = [
+	'pdfs' => 'uploads/pdf',
+	'pages' => 'uploads/img',
+	'signs' => 'uploads/sign',
+];
 
 require_once 'inc/mysql.php';
 
@@ -14,7 +18,7 @@ $extension = "(pdf|png)";
 
 echo "[".date("Y-m-d H:i:s")."]------------------------------------------------\n";
 
-function purge_dir($root_dir,$dir){
+function purge_dir($type, $root_dir, $dir){
 	global $db;
 	global $local_dir,$extension;
 	
@@ -24,28 +28,41 @@ function purge_dir($root_dir,$dir){
 		while($local_filename = readdir($local_filelist)){
 			if(!preg_match('/^\./', $local_filename)){
 				if(is_dir($local_dir . '/' . $root_dir . $dir . '/' . $local_filename)){
-					purge_dir($root_dir, $dir . '/' . $local_filename);
+					purge_dir($type, $root_dir, $dir . '/' . $local_filename);
 				}else{
 					if(preg_match('/^([a-f0-9]{16})([0-9\-]*)\.' . $extension . '$/', $local_filename, $regs)){
 						list(, $prefix, $index)=$regs;
-						$local_filedate = filemtime($local_dir . '/' . $root_dir . $dir . '/' . $local_filename);
+						$full_path = $local_dir . '/' . $root_dir . $dir . '/' . $local_filename;
+						$local_filedate = filemtime($full_path);
 						$str_filedate = date('Y-m-d H:i:s', $local_filedate);
 						# pour supprimer un fichier il faut
 						# - qu'il date de 24 heures au moins
 						# - qu'il n'appartienne pas à un utilisateur enregistré
 						if($local_filedate < (time() - 24 * 60 * 60)){
 
-							$used = false;
-							
 							$used = true;
-							$sql = "select * from `sign-a-pdf`.`docs` where doc_pdf_id = '{$prefix}'";
-							//echo "[{$sql}]\n";
-							$res = db_query($sql);
-							if($res != false){
-								if(db_num_rows($res) == 0){
-									$used = false;
-								} else{
-									//echo "preserved: {$str_filedate} {$prefix} {$index}\n";
+
+							if($type == 'signs') {
+								$sql = "select * from `sign-a-pdf`.`signs` where sign_file_id = '{$prefix}'";
+								//echo "[{$sql}]\n";
+								$res = db_query($sql);
+								if($res != false){
+									if(db_num_rows($res) == 0){
+										$used = false;
+									} else{
+										//echo "preserved: {$str_filedate} {$full_path}\n";
+									}
+								}
+							} else {
+								$sql = "select * from `sign-a-pdf`.`docs` where doc_pdf_id = '{$prefix}'";
+								//echo "[{$sql}]\n";
+								$res = db_query($sql);
+								if($res != false){
+									if(db_num_rows($res) == 0){
+										$used = false;
+									} else{
+										//echo "preserved: {$str_filedate} {$full_path}\n";
+									}
 								}
 							}
 							
@@ -65,8 +82,8 @@ function purge_dir($root_dir,$dir){
 	echo $root_dir.$dir . " {$count} files, {$deleted} deleted\n";
 }
 
-foreach($dir_list as $dir){
-	purge_dir($dir, '');
+foreach($dir_list as $type => $dir){
+	purge_dir($type, $dir, '');
 }
 
 db_close();
