ea724839f171b33ffc15fd7a400fac34afd75135
lal123 <alain23@gmail.com>
2026-04-19 02:00:48 +0200

    fix prev signs list (2)

--- inc/content/sign-doc-step2.php
+++ inc/content/sign-doc-step2.php
@@ -53,10 +53,10 @@ switch($sign_option) {
             $first = true;
             foreach($signs as $sign_file_id => $details) {
                 echo '<div class="form-check mb-2">';
-                echo '<input class="form-check-input" type="radio" name="sign_id" value="' . $sign_file_id . '"' . ((isset($sign_id) && ($sign_id == $sign_file_id)) || ((!isset($sign_id) || ($sign_id == '')) && ($first == true)) ? ' checked="checked"' : '') .'/>';
+                echo '<input class="form-check-input" type="radio" name="sign_id" id="sign_' . $sign_file_id . '" value="' . $sign_file_id . '"' . ((isset($sign_id) && ($sign_id == $sign_file_id)) || ((!isset($sign_id) || ($sign_id == '')) && ($first == true)) ? ' checked="checked"' : '') .'/>';
                 //echo '&nbsp; ';
                 echo '<label for="sign_' . $sign_file_id . '">#' . date($details['order']);
-                echo '<div class="prev-sign-preview" id="sign_' . $sign_file_id . '">';
+                echo '<div class="prev-sign-preview">';
                 echo '<img src="/' . UPLOAD_DIR . '/sign/' . $sign_file_id . '.png" alt="" border="0" />';
                 echo '</div>';
                 echo '</label>';
