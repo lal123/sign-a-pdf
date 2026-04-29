d174701c5b1adaf6441c86839f34f8b8e6130cae
lal123 <alain23@gmail.com>
2026-04-29 20:17:41 +0200

    new purge version including pages

--- admin/purge.php
+++ admin/purge.php
@@ -4,9 +4,9 @@ chdir('/var/www/sign-a-pdf');
 
 #liste des dossiers à purger
 $dir_list = [
-	'pdfs' => 'uploads/pdf',
-	'pages' => 'uploads/img',
-	'signs' => 'uploads/sign',
+	'pdfs' 	=> 'uploads/pdf',
+	'pages'	=> 'uploads/img',
+	'signs'	=> 'uploads/sign',
 ];
 
 require_once 'inc/mysql.php';
@@ -30,8 +30,8 @@ function purge_dir($type, $root_dir, $dir){
 				if(is_dir($local_dir . '/' . $root_dir . $dir . '/' . $local_filename)){
 					purge_dir($type, $root_dir, $dir . '/' . $local_filename);
 				}else{
-					if(preg_match('/^([a-f0-9]{16})([0-9\-]*)\.' . $extension . '$/', $local_filename, $regs)){
-						list(, $prefix, $index)=$regs;
+					if(preg_match('/^([a-f0-9]{16})([0-9\-\_]*)\.' . $extension . '$/', $local_filename, $regs)){
+						list(, $prefix, $index) = $regs;
 						$full_path = $local_dir . '/' . $root_dir . $dir . '/' . $local_filename;
 						$local_filedate = filemtime($full_path);
 						$str_filedate = date('Y-m-d H:i:s', $local_filedate);
@@ -52,6 +52,21 @@ function purge_dir($type, $root_dir, $dir){
 									} else{
 										//echo "preserved: {$str_filedate} {$full_path}\n";
 									}
+								} else {
+									echo "*** SQL ERROR *** [{$sql}]\n";
+								}
+							} else if($type == 'pages') {
+								$sql = "select * from `sign-a-pdf`.`pages` where page_id = '{$prefix}{$index}' /*and page_available = 1*/";
+								$res = db_query($sql);
+								if($res != false){
+									if(db_num_rows($res) == 0){
+										$used = false;
+										//echo "deleted: {$str_filedate} {$full_path}\n";
+									} else{
+										//echo "preserved: {$str_filedate} {$full_path}\n";
+									}
+								} else {
+									echo "*** SQL ERROR *** [{$sql}]\n";
 								}
 							} else {
 								$sql = "select * from `sign-a-pdf`.`docs` where doc_pdf_id = '{$prefix}'";
@@ -63,6 +78,8 @@ function purge_dir($type, $root_dir, $dir){
 									} else{
 										//echo "preserved: {$str_filedate} {$full_path}\n";
 									}
+								} else {
+									echo "*** SQL ERROR *** [{$sql}]\n";
 								}
 							}
 							
